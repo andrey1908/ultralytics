aca8df6b88de40d39aab172dcdf0b67ce4ee4f7e
andrey1908 <andrey.krishtopik@mail.ru>
2023-06-28 16:08:57 +0300

    ultralytics/yolo/utils/visualization.py: draw_results() function can accept palette to use for visualization

--- ultralytics/yolo/utils/visualization.py
+++ ultralytics/yolo/utils/visualization.py
@@ -3,7 +3,7 @@ import cv2
 from ultralytics.yolo.utils.ops import scale_image
 
 
-def draw_results(image, results, min_score=0.7):
+def draw_results(image, results, min_score=0.7, palette=((0, 0, 255),)):
     assert len(results) == 1
     result = results[0]
 
@@ -26,11 +26,13 @@ def draw_results(image, results, min_score=0.7):
     scaled_masks = scaled_masks.transpose(2, 0, 1)
 
     overlay = image.copy()
-    for score, class_id, scaled_mask in zip(scores, classes_ids, scaled_masks):
+    for i, (score, class_id, scaled_mask) in enumerate(zip(scores, classes_ids, scaled_masks)):
         if score < min_score:
             continue
         polygons, _ = cv2.findContours(scaled_mask,
             cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.polylines(image, polygons, True, (0, 0, 255), thickness=2)
-        overlay[scaled_mask != 0] = np.array([0, 0, 255], dtype=np.uint8)
+
+        color = palette[i % len(palette)]
+        cv2.polylines(image, polygons, True, color, thickness=2)
+        overlay[scaled_mask != 0] = np.array(color, dtype=np.uint8)
     cv2.addWeighted(image, 0.7, overlay, 0.3, 0, dst=image)
