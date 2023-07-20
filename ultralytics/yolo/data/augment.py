f8ff9f6b5947a31a7cfba6a7d02001fb13a63e3e
andrey1908 <andrey.krishtopik@mail.ru>
2023-07-20 22:49:55 +0300

    adjust blur augmentation (blur prob - 0.5, median blur prob - 0.2)

--- ultralytics/yolo/data/augment.py
+++ ultralytics/yolo/data/augment.py
@@ -558,8 +558,8 @@ class Albumentations:
             check_version(A.__version__, '1.0.3', hard=True)  # version requirement
 
             T = [
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
+                A.Blur(p=0.5),
+                A.MedianBlur(p=0.2),
                 A.ToGray(p=0.01),
                 A.CLAHE(p=0.01),
                 A.RandomBrightnessContrast(p=0.0),
@@ -570,8 +570,8 @@ class Albumentations:
             LOGGER.info(prefix + ', '.join(f'{x}'.replace('always_apply=False, ', '') for x in T if x.p))
         except ImportError:  # package not installed, skip
             pass
-        except Exception as e:
-            LOGGER.info(f'{prefix}{e}')
+        except:
+            raise
 
     def __call__(self, labels):
         im = labels['img']
